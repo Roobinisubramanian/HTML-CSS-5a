bb4bc20e0e6efebc1b2c064884e6041dab662d60
Roobinisubramanian <165560042+Roobinisubramanian@users.noreply.github.com>
2024-05-30 17:30:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function main1(){
+    var First = document.getElementById("First").value;
+    var last = document.getElementById("last").value;
+    var Email = document.getElementById("Email").value;
+    var Additionalcomments = document.getElementById("Additionalcomments").value;
+    
+    console.log(First);
+    console.log(last);
+    console.log(Email);
+    console.log(Additionalcomments);
+
+}
